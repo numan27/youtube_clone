d14d439903938921b442ba8703f2a8376b51f767
Numan Khalil <98921356+numan27@users.noreply.github.com>
2023-07-07 17:22:33 +0500

    final fixes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -14,7 +14,7 @@ import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
 import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
 
 
-export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
+export const logo = '/src/images/logo.png';  
 
 export const categories = [
   {
